f1633586820c31fd629078abdcee2658a172cc95
mstfatay <ataymstf35@gmail.com>
2024-01-07 23:24:39 +0000

    update test script

--- blackout_poetry/word_run.py
+++ blackout_poetry/word_run.py
@@ -4,11 +4,11 @@ from blackout_poetry.word_corpus_search import WordCorpusSearch
 if __name__ == "__main__":
     prompt = "Here is a poem: \n\n"
 
-    with open("corpus/1.txt", "r", encoding="utf-8") as f:
+    with open("blackout_poetry/corpus/1.txt", "r", encoding="utf-8") as f:
         corpus_text = f.read()
 
     corpus = BlackoutPoetryWordCorpus(corpus_text)
     corpus_search = WordCorpusSearch(corpus)
     result = corpus_search.corpus_search(
-        prompt, 50, output_file_path=f"output/run.json"
+        prompt, 50, output_file_path="blackout_poetry/output/run.json"
     )
